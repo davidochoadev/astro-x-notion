62c23595f3a84a2d9138d9c752ed533d38c797d1
davideochoaa <davideochoaa@gmail.com>
2023-06-23 02:08:29 +0200

    ✏️ Refactoring: Created 3 categories

--- src/components/MenuComponent.tsx
+++ src/components/MenuComponent.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 
-export function MenuComponent({ menu, other }) {
+export function MenuComponent({ sandwiches, drink, other }) {
   const [sandwichIsView, setSandwichIsView] = useState(true);
   const [drinkIsView, setDrinkIsView] = useState(false);
+  const [otherIsView, setOtherIsView] = useState(false);
 
   return (
     <main className="grid gap-4 grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 mx-auto max-w-screen-2xl px-4 sm:px-6 min-h-[calc(100lvh-80px)]">
@@ -31,6 +32,7 @@ export function MenuComponent({ menu, other }) {
               if (sandwichIsView === false) {
                 setSandwichIsView(true);
                 setDrinkIsView(false);
+                setOtherIsView(false);
               }
             }}
           >
@@ -49,6 +51,7 @@ export function MenuComponent({ menu, other }) {
               if (drinkIsView === false) {
                 setSandwichIsView(false);
                 setDrinkIsView(true);
+                setOtherIsView(false);
               }
             }}
           >
@@ -57,9 +60,20 @@ export function MenuComponent({ menu, other }) {
               Drinks
             </p>
           </button>
-          <button className="rounded-xl p-3 bg-zinc-700 flex flex-row gap-2">
+          <button
+            className={`rounded-xl p-3 ${
+              otherIsView === true ? "bg-orange-400" : "bg-zinc-700"
+            }  flex flex-row gap-2`}
+            onClick={() => {
+              if (otherIsView === false) {
+                setSandwichIsView(false);
+                setDrinkIsView(false);
+                setOtherIsView(true);
+              }
+            }}
+          >
             <p className="text-xl">🍱</p>
-            <p className="text-lg">Others</p>
+            <p className={`${otherIsView ? "text-slate-700" : "text-white"}`}>Others</p>
           </button>
         </div>
       </aside>
@@ -71,19 +85,19 @@ export function MenuComponent({ menu, other }) {
                 <p className="text-3xl font-bold text-orange-200">Sandwich</p>
               </div>
             </div>
-            {menu.map((item, index) => (
+            {sandwiches.map((item, index) => (
               <div
-                className="flex min-w-[18rem] flex-1 flex-col items-center justify-start gap-2 rounded-xl border border-zinc-700 bg-zinc-800 p-6"
+                className="flex min-w-[18rem] flex-1 flex-col items-center justify-start gap-2 rounded-xl border border-gray-700 bg-gray-800 p-6"
                 key={index}
               >
                 <div className="flex h-[200px] w-[200px] items-center justify-center rounded-xl bg-white">
                   {item.img !== "" ? (
                     <img src={item?.img} alt="Food" width={200} height={200} />
                   ) : (
-                    <img src="../assets/salda.webp" alt="" />
+                    <img src="../assets/salad.webp" alt="" />
                   )}
                 </div>
-                <div className="text-center">
+                <div>
                   <h4 className="my-2 flex text-2xl font-bold text-gray-100">
                     {item?.title !== "" ? item?.title : "N/A"}
                   </h4>
@@ -92,7 +106,7 @@ export function MenuComponent({ menu, other }) {
                       {item?.description}
                     </p>
                   )}
-                  <div className="pt-2">
+                  <div>
                     {item?.isAvailable ? (
                       <div className=" my-2 flex w-min flex-row items-center gap-2 rounded-xl bg-green-300 p-1 px-2 text-xs">
                         <div className="h-[5px] w-[5px] rounded-full bg-green-500" />
@@ -122,51 +136,108 @@ export function MenuComponent({ menu, other }) {
           <>
             <div className="flex items-center mb-8 w-full">
               <div className="flex-1">
+                <p className="text-3xl font-bold text-orange-200">Drinks</p>
+              </div>
+            </div>
+            {drink.map((item, index) => (
+              <div
+                className="flex min-w-[18rem] flex-1 flex-col items-center justify-start gap-2 rounded-xl border border-gray-700 bg-gray-800 p-6"
+                key={index}
+              >
+                <div className="flex h-[200px] w-[200px] items-center justify-center rounded-xl bg-white">
+                  {item.img !== "" ? (
+                    <img src={item?.img} alt="Food" width={200} height={200} />
+                  ) : (
+                    <img src="../assets/salad.webp" alt="" />
+                  )}
+                </div>
+                <div>
+                  <h4 className="my-2 flex text-2xl font-bold text-gray-100">
+                    {item?.title !== "" ? item?.title : "N/A"}
+                  </h4>
+                  {item?.description !== "" && (
+                    <p className="text-md my-2 text-gray-400">
+                      {item?.description}
+                    </p>
+                  )}
+                  <div>
+                    {item?.isAvailable ? (
+                      <div className=" my-2 flex w-min flex-row items-center gap-2 rounded-xl bg-green-300 p-1 px-2 text-xs">
+                        <div className="h-[5px] w-[5px] rounded-full bg-green-500" />
+                        <p className="text-gray-800">Available</p>
+                      </div>
+                    ) : (
+                      <div className=" my-2 flex w-max flex-row items-center gap-2 rounded-xl bg-red-300 p-1 px-2 text-xs">
+                        <div className="h-[5px] w-[5px] rounded-full bg-red-500" />
+                        <p className="text-gray-800">Not Available</p>
+                      </div>
+                    )}
+                  </div>
+                  <p className="text-md text-amber-400">
+                    {item?.price
+                      ? item?.price.toLocaleString("en-US", {
+                          style: "currency",
+                          currency: "USD",
+                        })
+                      : "N/A"}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </>
+        )}
+        {otherIsView && (
+          <>
+            <div className="flex items-center mb-8 w-full">
+              <div className="flex-1">
                 <p className="text-3xl font-bold text-orange-200">Other</p>
               </div>
             </div>
-            {
-				other.map((item, index) => (
-					<div className="flex min-w-[18rem] flex-1 flex-col items-center justify-start gap-2 rounded-xl border border-gray-700 bg-gray-800 p-6" key={index}>
-						<div className="flex h-[200px] w-[200px] items-center justify-center rounded-xl bg-white">
-							{item.img !== '' ? (
-								<img src={item?.img} alt="Food" width={200} height={200}/>
-							) : (
-								<img src="../assets/salad.webp" alt=""/>
-							)}
-						</div>
-						<div>
-							<h4 className="my-2 flex text-2xl font-bold text-gray-100">
-								{item?.title !== '' ? item?.title : 'N/A'}
-							</h4>
-							{item?.description !== '' && (
-								<p className="text-md my-2 text-gray-400">{item?.description}</p>
-							)}
-							<div>
-								{item?.isAvailable ? (
-									<div className=" my-2 flex w-min flex-row items-center gap-2 rounded-xl bg-green-300 p-1 px-2 text-xs">
-										<div className="h-[5px] w-[5px] rounded-full bg-green-500" />
-										<p className="text-gray-800">Available</p>
-									</div>
-								) : (
-									<div className=" my-2 flex w-max flex-row items-center gap-2 rounded-xl bg-red-300 p-1 px-2 text-xs">
-										<div className="h-[5px] w-[5px] rounded-full bg-red-500" />
-										<p className="text-gray-800">Not Available</p>
-									</div>
-								)}
-							</div>
-							<p className="text-md text-amber-400">
-								{item?.price
-									? item?.price.toLocaleString('en-US', {
-											style: 'currency',
-											currency: 'USD'
-									  })
-									: 'N/A'}
-							</p>
-						</div>
-					</div>
-				))
-			}
+            {other.map((item, index) => (
+              <div
+                className="flex min-w-[18rem] flex-1 flex-col items-center justify-start gap-2 rounded-xl border border-gray-700 bg-gray-800 p-6"
+                key={index}
+              >
+                <div className="flex h-[200px] w-[200px] items-center justify-center rounded-xl bg-white">
+                  {item.img !== "" ? (
+                    <img src={item?.img} alt="Food" width={200} height={200} />
+                  ) : (
+                    <img src="../assets/salad.webp" alt="" />
+                  )}
+                </div>
+                <div>
+                  <h4 className="my-2 flex text-2xl font-bold text-gray-100">
+                    {item?.title !== "" ? item?.title : "N/A"}
+                  </h4>
+                  {item?.description !== "" && (
+                    <p className="text-md my-2 text-gray-400">
+                      {item?.description}
+                    </p>
+                  )}
+                  <div>
+                    {item?.isAvailable ? (
+                      <div className=" my-2 flex w-min flex-row items-center gap-2 rounded-xl bg-green-300 p-1 px-2 text-xs">
+                        <div className="h-[5px] w-[5px] rounded-full bg-green-500" />
+                        <p className="text-gray-800">Available</p>
+                      </div>
+                    ) : (
+                      <div className=" my-2 flex w-max flex-row items-center gap-2 rounded-xl bg-red-300 p-1 px-2 text-xs">
+                        <div className="h-[5px] w-[5px] rounded-full bg-red-500" />
+                        <p className="text-gray-800">Not Available</p>
+                      </div>
+                    )}
+                  </div>
+                  <p className="text-md text-amber-400">
+                    {item?.price
+                      ? item?.price.toLocaleString("en-US", {
+                          style: "currency",
+                          currency: "USD",
+                        })
+                      : "N/A"}
+                  </p>
+                </div>
+              </div>
+            ))}
           </>
         )}
       </div>
