0cadcd5f3f60d2d332894bbeb2af3eef3b0d54f4
davideochoaa <davideochoaa@gmail.com>
2023-06-26 14:45:26 +0200

    ✅ Feat: Changed style and create custom theme

--- src/components/MenuComponent.tsx
+++ src/components/MenuComponent.tsx
@@ -25,14 +25,14 @@ export function MenuComponent({ sandwiches, categories }) {
 
   return (
     <main className="grid gap-4 grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 mx-auto max-w-screen-2xl px-2 sm:px-6 min-h-[calc(100lvh-80px)]">
-      <aside className="w-full align-middle sm:sticky pt-10 sm:h-1 sm:top-0 sm:mt-24 sm:pt-4 sm:w-full p-4">
+      <aside className="w-full align-middle sm:sticky pt-10 sm:h-1 sm:top-6 sm:mt-24 sm:pt-4 sm:w-full p-2">
         <div className="flex items-center mb-10">
           <div className="flex-1">
-            <p className="text-3xl font-bold text-orange-200">Menu</p>
+            <p className="text-3xl font-bold text-selected">Menu</p>
           </div>
         </div>
         <div
-          className="flex flex-row gap-4 overflow-x-auto py-4 px-4 sm:overflow-y-auto sm:flex-col rounded-xl bg-gray-800 border border-gray-700 max-h-[calc(100dvh-250px)]"
+          className="flex flex-row gap-4 overflow-x-auto py-4 px-4 sm:overflow-y-auto sm:flex-col rounded-xl bg-secondary border border-border max-h-[calc(100dvh-250px)] shadow-md "
           id="mobile-menu"
         >
          {/* CHECK AVAILABLE ITEMS */}
@@ -42,16 +42,16 @@ export function MenuComponent({ sandwiches, categories }) {
                key={index}
                className={`rounded-xl p-3 ${
                  activeCategory === category
-                   ? "bg-orange-400"
-                   : "bg-zinc-700 hover:bg-orange-400"
+                   ? " bg-selected-area"
+                   : " bg-non-selected-area hover:bg-selected-area"
                } group min-w-max`}
                onClick={() => handleCategoryClick(category)}
              >
                <p
                  className={`${
                    activeCategory === category
-                     ? "text-slate-700"
-                     : "text-white group-hover:text-slate-700"
+                     ? "text-selected"
+                     : "text-non-selected group-hover:text-selected"
                  } `}
                >
                  {category}
@@ -61,12 +61,12 @@ export function MenuComponent({ sandwiches, categories }) {
           ))}
         </div>
       </aside>
-      <div className="flex flex-row flex-wrap gap-2 p-4 sm:col-span-2 lg:col-span-3 2xl:col-span-4 sm:pt-28 pb-28">
+      <div className="flex flex-row flex-wrap gap-2 p-2 sm:p-4 sm:col-span-2 lg:col-span-3 2xl:col-span-4 sm:pt-28 pb-28">
         {activeCategory && (
           <>
             <div className="flex items-center mb-8 w-full">
               <div className="flex-1">
-                <p className="text-3xl font-bold text-orange-200">{getCategoryName(activeCategory)}</p>
+                <p className="text-3xl font-bold text-selected">{getCategoryName(activeCategory)}</p>
               </div>
             </div>
             {filtered.map((item, index) => (
@@ -87,7 +87,7 @@ export function MenuComponent({ sandwiches, categories }) {
 
 export function Card({ title, description, isAvailable, price }) {
   return (
-    <div className="flex min-w-[18rem] flex-1 flex-col items-start justify-start gap-2 rounded-xl border border-gray-700 bg-gray-800 p-6">
+    <div className="flex min-w-[18rem] flex-1 flex-col items-start justify-start gap-2 rounded-xl border border-border bg-secondary p-6 shadow-md ">
       {/*                 <div className="flex h-[200px] w-[200px] items-center justify-center rounded-xl bg-white">
       {item.img !== "" ? (
         <img src={item?.img} alt="Food" width={200} height={200} />
@@ -96,26 +96,23 @@ export function Card({ title, description, isAvailable, price }) {
       )}
     </div> */}
       <div>
-        <h4 className="my-2 flex text-2xl font-bold text-gray-100">
+        <h4 className="my-2 flex text-2xl font-bold text-dark-text">
           {title !== "" ? title : "N/A"}
         </h4>
-        {description !== "" && (
-          <p className="text-md my-2 text-gray-400">{description}</p>
-        )}
         <div>
           {isAvailable ? (
-            <div className=" my-2 flex w-min flex-row items-center gap-2 rounded-xl bg-green-300 p-1 px-2 text-xs">
-              <div className="h-[5px] w-[5px] rounded-full bg-green-500" />
-              <p className="text-gray-800">Available</p>
+            <div className=" my-2 flex w-min flex-row items-center gap-2 rounded-xl bg-available-area p-1 px-2 text-xs">
+              <div className="h-[5px] w-[5px] rounded-full bg-available" />
+              <p className="text-available">Available</p>
             </div>
           ) : (
-            <div className=" my-2 flex w-max flex-row items-center gap-2 rounded-xl bg-red-300 p-1 px-2 text-xs">
-              <div className="h-[5px] w-[5px] rounded-full bg-red-500" />
+            <div className=" my-2 flex w-max flex-row items-center gap-2 rounded-xl bg-not-available-area p-1 px-2 text-xs">
+              <div className="h-[5px] w-[5px] rounded-full bg-not-available" />
               <p className="text-gray-800">Not Available</p>
             </div>
           )}
         </div>
-        <p className="text-md text-amber-400">
+        <p className="text-md text-selected">
           {price
             ? price.toLocaleString("en-US", {
                 style: "currency",
@@ -123,6 +120,9 @@ export function Card({ title, description, isAvailable, price }) {
               })
             : "N/A"}
         </p>
+        {description !== "" && (
+          <p className="text-md my-2 text-text">{description}</p>
+        )}
       </div>
     </div>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,22 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			'main': '#fafafa',
+			'secondary' : '#ffffff',
+			'border' : '#d6d6d6',
+			'text' : '#7f7f7f',
+			'dark-text' : '#3c3c3c',
+			'selected' : '#ec7905',
+			'selected-area': '#feefe1',
+			'alternate' : '#ffffff',
+			'non-selected' : '#959595',
+			'non-selected-area' : '#ebebeb',
+			'available-area': '#E6FFE1',
+			'available': '#00b600',
+			'not-available-area' : '#FDCBCB',
+			'not-available' : '#FD7A7A',
+		 },
 		extend: {},
 	},
 	plugins: [],
